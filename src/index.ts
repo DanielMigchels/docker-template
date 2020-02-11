c34d4e0936fd3d92c804d5730f9d483ef7d1d0b7
Fdebijl <18357629+Fdebijl@users.noreply.github.com>
2020-02-11 13:42:43 +0100

    Make the container persistent

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
-// Give something for TSC to process:
+import moment from 'moment';
+
+const INTERVAL = 10;
+
 const logger = (text: string): void => {
-  console.log(text);
+  console.log(`${moment().format('DD/MM/Y HH:mm:ss')} - ${text}`);
 };
 
-logger('Hello World');
+let i = 1;
+
+setInterval(() => {
+  logger(`This container has been alive for ${i * INTERVAL} seconds.`);
+}, INTERVAL * 1000);
